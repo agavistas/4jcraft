0bd089b1aebbeef4e469f53496eb52665df72034
agavistas <agavistas@users.noreply.github.com>
2026-03-29 03:06:02 +0000

    deploy: fd90607719c44675ac57b8398b935ffb56723800

---
+++ navtreedata.js
@@ -0,0 +1,87 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "4JCraft", "index.html", [
+    [ "Welcome to the 4JCraft Docs!", "index.html", null ],
+    [ "Minecraft.Client Asset Structure", "md__2home_2runner_2work_24jcraft_24jcraft_2Minecraft_8Assets_2README.html", [
+      [ "Windows64Media.arc contents", "md__2home_2runner_2work_24jcraft_24jcraft_2Minecraft_8Assets_2README.html#autotoc_md1", null ],
+      [ "Asset locations", "md__2home_2runner_2work_24jcraft_24jcraft_2Minecraft_8Assets_2README.html#autotoc_md2", [
+        [ "Generic assets should be contained inside this folder:", "md__2home_2runner_2work_24jcraft_24jcraft_2Minecraft_8Assets_2README.html#autotoc_md3", null ],
+        [ "Platform assets are contained in Minecraft.Client/Platform/", "md__2home_2runner_2work_24jcraft_24jcraft_2Minecraft_8Assets_2README.html#autotoc_md4", null ]
+      ] ]
+    ] ],
+    [ "Network Code Implementation Notes", "md__2home_2runner_2work_24jcraft_24jcraft_2Minecraft_8Client_2Network_01Implementation_01Notes.html", [
+      [ "Overview", "md__2home_2runner_2work_24jcraft_24jcraft_2Minecraft_8Client_2Network_01Implementation_01Notes.html#autotoc_md6", [
+        [ "Notes", "md__2home_2runner_2work_24jcraft_24jcraft_2Minecraft_8Client_2Network_01Implementation_01Notes.html#autotoc_md7", null ]
+      ] ],
+      [ "Platform-Specific Files", "md__2home_2runner_2work_24jcraft_24jcraft_2Minecraft_8Client_2Network_01Implementation_01Notes.html#autotoc_md8", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"AABB_8cpp_source.html",
+"ByteBuffer_8cpp_source.html",
+"Cube_8cpp_source.html",
+"Enemy_8cpp_source.html",
+"GroundBushFeature_8h_source.html",
+"LeashItem_8h_source.html",
+"MoveTowardsRestrictionGoal_8cpp_source.html",
+"PrimedTnt_8h_source.html",
+"SignRenderer_8h_source.html",
+"TemptGoal_8h_source.html",
+"UIScene__ControlsMenu_8cpp_source.html",
+"WstringLookup_8cpp_source.html",
+"classArchiveFile.html#a591fd928107e465c18c3fe703cb16a03",
+"classChicken.html",
+"classEnderCrystalModel.html",
+"classIUIScene__HorseInventoryMenu.html",
+"classMultiplayerLocalPlayer.html",
+"classServerInterface.html",
+"classThrownPotion.html",
+"classXboxStructureActionGenerateBox.html",
+"structAUDIO__INFO.html",
+"struct__EnchantingScreenInput.html",
+"zconf_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ navtreeindex22.js
@@ -0,0 +1,7 @@
+var NAVTREEINDEX22 =
+{
+"zconf_8h_source.html":[4,0,1,5,0,11,22],
+"zlib_8h_source.html":[4,0,1,5,0,11,23],
+"zutil_8c_source.html":[4,0,1,5,0,11,24],
+"zutil_8h_source.html":[4,0,1,5,0,11,25]
+};
